430707a1ad7e594a0659f1e14037c80f9a682728
Mayank Sharma <mayank.nimcet.188@gmail.com>
2021-02-03 15:04:14 +0530

    Added Article Model and Article Schema using Marshmallow

--- app.py
+++ app.py
@@ -14,7 +14,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 #Init masrhmallow
 ma = Marshmallow(app)
+#Article Model
+class Article(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    author = db.Column(db.String(64))
+    title = db.Column(db.String(100),unique=True)
+    content = db.Column(db.Text)
 
+    def __init__(self,author,title,content):
+        self.author = author
+        self.title = title
+        self.content = content
+
+    def __repr__(self):
+        return '< Article %r >' % self.title
+
+#Creating the article schema using marshmallow
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id','author','title','content')
+
+#Initializing the schema
+#For a single article
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
 
 #Run the flask server
 if __name__ == '__main__':
